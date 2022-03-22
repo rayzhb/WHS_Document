ce39e9b84cf45471d750d07516806d1ad22ce127
Ray <bbaxx1314@qq.com>
2022-03-22 17:02:39 +0800

    docsearch add chinese language

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -122,40 +122,50 @@ module.exports = {
         apiKey: '796955fbbe154ad615a249f76ea5f894',
         indexName: 'whs-ray-zhb',
         locales: {
-          '/en/': {
-            placeholder: 'Search Documentation',
-            translations: {
-              button: {
-                buttonText: 'Search Documentation',
-              },
-            },
-          },
           '/': {
             placeholder: '搜索文档',
             translations: {
               button: {
                 buttonText: '搜索文档',
+                buttonAriaLabel: '搜索文档',
+              },
+              modal: {
+                searchBox: {
+                  resetButtonTitle: '清除查询条件',
+                  resetButtonAriaLabel: '清除查询条件',
+                  cancelButtonText: '取消',
+                  cancelButtonAriaLabel: '取消',
+                },
+                startScreen: {
+                  recentSearchesTitle: '搜索历史',
+                  noRecentSearchesText: '没有搜索历史',
+                  saveRecentSearchButtonTitle: '保存至搜索历史',
+                  removeRecentSearchButtonTitle: '从搜索历史中移除',
+                  favoriteSearchesTitle: '收藏',
+                  removeFavoriteSearchButtonTitle: '从收藏中移除',
+                },
+                errorScreen: {
+                  titleText: '无法获取结果',
+                  helpText: '你可能需要检查你的网络连接',
+                },
+                footer: {
+                  selectText: '选择',
+                  navigateText: '切换',
+                  closeText: '关闭',
+                  searchByText: '搜索提供者',
+                },
+                noResultsScreen: {
+                  noResultsText: '无法找到相关结果',
+                  suggestedQueryText: '你可以尝试查询',
+                  openIssueText: '你认为该查询应该有结果？',
+                  openIssueLinkText: '点击反馈',
+                },
               },
             },
           },
         },
       },
     ],
-    // [
-    //   '@vuepress/plugin-search',
-    //   {
-    //    // 排除首页
-    //     isSearchable: (page) => page.path !== '/',
-    //     locales: {
-    //       '/': {
-    //         placeholder: '搜索',
-    //       },
-    //       '/en/': {
-    //         placeholder: 'Search',
-    //       },
-    //     },
-    //   },
-    // ],
     [
       '@vuepress/pwa',
       {
